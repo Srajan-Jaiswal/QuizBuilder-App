6fd7366cb9ce41f1275041bea27870c7fc5e7fea
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-09-06 21:53:19 +0530

    Update Auth.dart

--- lib/services/Auth.dart
+++ lib/services/Auth.dart
@@ -1,5 +1,13 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:quizbuilder/models/user.dart';
 
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  // function checking for the anonymys users
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(user.uid) : null;
+  }
   Future signInEmailAndPass(String email, String password) async {
     try {
       AuthResult authResult = await _auth.signInWithEmailAndPassword(
