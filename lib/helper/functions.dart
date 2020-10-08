d699515898df8cf55fceeb82b958c216b17056e9
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-10-08 14:58:10 +0100

    Update functions.dart

--- lib/helper/functions.dart
+++ lib/helper/functions.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+//  helper function class
 class HelperFunctions {
   static String userLoggrdInKey = "USERLOGGEDINKEY";
   static saveUserLoggedInDetails({@required bool isLoggedin}) async {
