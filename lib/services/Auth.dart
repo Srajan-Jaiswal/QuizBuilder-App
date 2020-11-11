6ea079f244ba3992032af73944b3fbbde5d27bf2
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-11-11 23:57:29 +0530

    Update Auth.dart

--- lib/services/Auth.dart
+++ lib/services/Auth.dart
@@ -19,6 +19,16 @@ class AuthService {
     }
   }
 
+    Future signUpEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult authResult = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser firebaseUser = authResult.user;
+      return _userFromFirebaseUser(firebaseUser);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   Future signOut() async {
     try {
